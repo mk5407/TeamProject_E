45eeaf6b273942feff25fd7c818517dfedcea02c
whit44 <kj.whit44@gmail.com>
2022-03-02 20:48:45 +0900

    Operation type Enum 생성

---
+++ Visual_Project/Operation.h
@@ -0,0 +1,24 @@
+#pragma once
+
+enum class OPCode {
+	OP_ADD,
+	OP_DEL,
+	OP_SCH,
+	OP_MOD,
+};
+
+enum class OPOption1 {
+	OP1_PRINT,
+	OP1_NONE,
+};
+
+enum class OPOption2 {
+	OP2_NAME_FIRSTNAME,
+	OP2_NAME_LASTNAME,
+	OP2_PHONE_MIDDLE,
+	OP2_PHONE_LAST,
+	OP2_BIRTH_YEAR,
+	OP2_BIRTH_MONTH,
+	OP2_BIRTH_DAY,
+};
+
