4b9423e0810f81691d30198229f2859b425f0a67
PolarisCr <kimek12@gmail.com>
2022-03-03 10:53:29 +0900

    Employee 항목 변경
    Employee 논의 후 Name 분리 및 Phone Number 분리한 Employee
    변경하였습니다.
    추가로 Column Test 의 Name Test Fail 발생하여 Test Code 추가
    수정하였습니다.
    
    Signed-off-by: PolarisCr <kimek12@gmail.com>

--- Visual_Project/Employee.h
+++ Visual_Project/Employee.h
@@ -1,26 +1,74 @@
 #pragma once
 #include <string>
-enum class CareerLevel
+#include <vector>
+using namespace std;
+
+enum class Type
 {
-	CL1,
-	CL2,
-	CL3,
-	CL4
+	EmployeeNum = 4,
+	Name,
+	CL,
+	PhoneNum,
+	BirthDay,
+	Certi
 };
 
-enum class CertiLevel
+class Employee
 {
-	ADV,
-	PRO,
-	EX
-};
+public:
+	Employee(string empNum, string empName, string empCl, string empPhoneNum, string empBirthday, string empCerti) :
+		employeeNum(empNum), name(empName), cl(empCl), phoneNum(empPhoneNum), birthday(empBirthday), certi(empCerti)
+	{}
+	Employee(vector<string> vec) : employeeNum(vec[(int)Type::EmployeeNum]), name(vec[(int)Type::Name]), cl(vec[(int)Type::CL]),
+		                           phoneNum(vec[(int)Type::PhoneNum]), birthday(vec[(int)Type::BirthDay]), certi(vec[(int)Type::Certi])
+	{}
 
-struct Employee
-{
-	std::string employeeNum;
-	std::string name;
-	CareerLevel cl;
-	std::string phoneNum;
-	std::string birthday;
-	CertiLevel certi;
+	string getEmployeeNum()
+	{
+		return employeeNum;
+	}
+	string getName()
+	{
+		return name;
+	}
+	string getFirstName()
+	{
+		return name.substr(0, name.rfind(' '));
+	}
+	string getLastName()
+	{
+		return name.substr(name.rfind(' ') + 1);
+	}
+	string getCL()
+	{
+		return cl;
+	}
+	string getPhoneNum()
+	{
+		return phoneNum;
+	}
+	string getMiddlePhoneNum()
+	{
+		return phoneNum.substr(4, 4);
+	}
+	string getLastPhoneNum()
+	{
+		return phoneNum.substr(9, 4);
+	}
+	string getBirthday()
+	{
+		return birthday;
+	}
+	string getCerti()
+	{
+		return certi;
+	}
+
+private:
+	string employeeNum;
+	string name;
+	string cl;
+	string phoneNum;
+	string birthday;
+	string certi;
 };
