90e5f9c324e8cdd5a8748ed6722b15b15a469f13
mk5407 <minkyu5407@gmail.com>
2022-03-04 10:08:44 +0900

    [Feature]MOD 구현완료
    MOD(CMDline) -> MOD(CMDline, find_list)로 정책 변경됨에 따라 리팩토링 과정 필요합니다.

--- ModTest/mod_test.cpp
+++ ModTest/mod_test.cpp
@@ -9,9 +9,9 @@ using namespace std;
 
 TEST(ModInitTest, ModTest) {
   
-	Employee humon1 = { "15123099", "VXIHXOTH JHOP", CareerLevel::CL3, "010 - 3112 - 2609", "19771211", CertiLevel::ADV };
-	Employee humon2 = { "17112609", "FB NTAWR",      CareerLevel::CL4, "010 - 5645 - 6122", "19861203", CertiLevel::PRO};
-	Employee humon3 = { "14130827", "TTETHU HBO",    CareerLevel::CL3, "010 - 4581 - 2050", "20080718", CertiLevel::ADV};
+	Employee humon1 = { "15123099", "VXIHXOTH JHOP", "CL3", "010 - 3112 - 2609", "19771211", "ADV"};
+	Employee humon2 = { "17112609", "FB NTAWR",      "CL4", "010 - 5645 - 6122", "19861203", "PRO"};
+	Employee humon3 = { "14130827", "TTETHU HBO",    "CL3", "010 - 4581 - 2050", "20080718", "ADV"};
 
 	vector<Employee> employee_list;
 	employee_list.push_back(humon1);
@@ -21,16 +21,16 @@ TEST(ModInitTest, ModTest) {
 	//EXPECT_EQ(15123099, humon1.employeeNum);
 	//EXPECT_EQ("VXIHXOTH JHOP", humon1.name);
 
-	// employeeNum Test
+	//// employeeNum Test
 	Mod modObj2("MOD,-p, , ,employeeNum,14130827,birthday,20110706");
 	vector<Employee*> find_list2 = modObj2.getModifyList(employee_list);
-	string find_name = find_list2.front()->name;
+	string find_name = find_list2.front()->getName();
 	EXPECT_EQ("TTETHU HBO", find_name);
 
 	// name Test
 	Mod modObj("MOD,-p, , ,name,FB NTAWR,cl,CL3");
 	vector<Employee*> find_list = modObj.getModifyList(employee_list);
-	EXPECT_EQ("FB NTAWR", find_list.front()->name);
+	EXPECT_EQ("FB NTAWR", find_list.front()->getName());
 
 	// cl Test
 	Mod modObj3("MOD,-p, , ,cl,CL3,birthday,20110706");
@@ -40,15 +40,15 @@ TEST(ModInitTest, ModTest) {
 	// phoneNum Test
 	Mod modObj4("MOD,-p, , ,phoneNum,010 - 4581 - 2050,birthday,20110706");
 	vector<Employee*> find_list4 = modObj4.getModifyList(employee_list);
-	EXPECT_EQ("TTETHU HBO", find_list4.front()->name);
+	EXPECT_EQ("TTETHU HBO", find_list4.front()->getName());
 
 	// birthday Test
 	Mod modObj5("MOD,-p, , ,birthday,19771211,birthday,20110706");
 	vector<Employee*> find_list5 = modObj5.getModifyList(employee_list);
-	EXPECT_EQ("VXIHXOTH JHOP", find_list5.front()->name);
+	EXPECT_EQ("VXIHXOTH JHOP", find_list5.front()->getName());
 
 	// certi Test
 	Mod modObj6("MOD,-p, , ,certi,PRO,birthday,20110706");
 	vector<Employee*> find_list6 = modObj6.getModifyList(employee_list);
-	EXPECT_EQ("FB NTAWR", find_list6.front()->name);
+	EXPECT_EQ("FB NTAWR", find_list6.front()->getName());
 }
--- Visual_Project/Mod.cpp
+++ Visual_Project/Mod.cpp
@@ -7,8 +7,6 @@ using namespace std;
 
 Mod::Mod(string cmd) 
 {
-	option1_ = 0;
-	option2_ = 0;
 	parsingCMD(cmd);
 }
 
@@ -24,142 +22,92 @@ void Mod::parsingCMD(string cmd)
 		str_list.push_back(str_buf);
 	}
 
-	int type = 0;
+	option1_ = str_list[(int)InputType::Option1];
+	option2_ = str_list[(int)InputType::Option2];
+
+	int type = (int)Type::EmployeeNum;
 
 	vector<string> mod_type = { "employeeNum", "name", "cl", "phoneNum", "birthday", "certi" };
 
-	// [TO DO] Refactoring 
 	for (string mod_select : mod_type)
 	{
-		if (!mod_select.compare(str_list[4]))
+		if (!mod_select.compare(str_list[(int)InputType::Search_type]))
 		{
 			find_.type_ = static_cast<Type>(type);
-			find_.content_ = str_list[5];
+			find_.content_ = str_list[(int)InputType::Search_content];
 		}
 
-		if (!mod_select.compare(str_list[6]))
+		if (!mod_select.compare(str_list[(int)InputType::Modify_type]))
 		{
-			modify_.type_ = static_cast<Type>(type);;
-			modify_.content_ = str_list[7];
+			modify_.type_ = static_cast<Type>(type);
+			modify_.content_ = str_list[(int)InputType::Modify_content];
 		}
 		type++;
+	}	
+}
+string Mod::getEmployeeInfo(Employee* e, Type type)
+{
+	//[TO DO] Refactoring
+	string Option2Check[] = {"-f","-l","-m","-l"};
+	Type ModifyCheck[] = { Type::Name,Type::Name,Type::PhoneNum,Type::PhoneNum };
+
+	string option2_employeeInfo[] = { e->getFirstName(), e->getLastName(),e->getMiddlePhoneNum(),e->getLastPhoneNum() };
+
+	for (int i = 0; i < MAX_OPTION2_SIZE; i++)
+	{
+		string option2_check = Option2Check[i];
+		Type modify_check = ModifyCheck[i];
+
+		if ((option2_check == option2_) && (modify_check == modify_.type_))
+		{
+			return option2_employeeInfo[i];
+		}
 	}
 	
+	string employeeInfo[] = {e->getEmployeeNum(), e->getName(), e->getCL(), e->getPhoneNum(),e->getBirthday(), e->getCerti() };
+	
+	int index = (int)type - (int)Type::EmployeeNum;
+
+	return employeeInfo[index];
 }
 
 vector<Employee*> Mod::getModifyList(vector<Employee>& employee_list)
 {
-	// [TO DO] Refactoring 
-	CareerLevel find_cl;
-	vector<string> cl_type = { "CL1", "CL2", "CL3", "CL4" };
-	CertiLevel find_certi;
-	vector<string> certi_type = { "ADV", "PRO", "EX" };
-
-	int cl_num = 0;
-	int certi_num = 0;
-
-	for (string cl_str : cl_type)
-	{
-		if (cl_str == find_.content_)
-		{
-			find_cl = static_cast<CareerLevel>(cl_num);
-		}
-		cl_num++;
-	}
-
-	for (string certi_str : certi_type)
-	{
-		if (certi_str == find_.content_)
-		{
-			find_certi = static_cast<CertiLevel>(certi_num);
-		}
-		certi_num++;
-	}
-
 	int vector_size = employee_list.size();
 
-	// [TO DO] Refactoring 
-	switch (find_.type_)
+	for (int i = 0; i < vector_size; i++)
 	{
-	case Type::EmployeeNum:
+		Employee* e = &employee_list[i];
 
-		for (int i = 0; i< vector_size; i++)
+		if (getEmployeeInfo(e, find_.type_) == find_.content_)
 		{
-			Employee* e = &employee_list[i];
-
-			if (e->employeeNum == find_.content_)
-			{
-				find_list_.push_back(e);
-			}
+			find_list_.push_back(e);
 		}
+	}
 
-		break;
-	case Type::Name:
-		
-		for (int i = 0; i < vector_size; i++)
+	//[TO DO] Refactoring
+	if (option1_ != "-p")
+	{
+		result_string_.push_back("MOD,"+to_string(find_list_.size()));
+	}
+	else
+	{
+		int find_size = find_list_.size();
+
+		for (Employee *e : find_list_)
 		{
-			Employee* e = &employee_list[i];
+			string output="MOD,";
 
-			if (e->name == find_.content_)
+			for (int i = EMPLOYEE_INFO_START; i<= EMPLOYEE_INFO_END; i++)
 			{
-				find_list_.push_back(e);
+				Type type = static_cast<Type>(i);
+
+				output += getEmployeeInfo(e, type);
+				output += ",";
 			}
+
+			result_string_.push_back(output);
 		}
-		break;
-
-	case Type::CL:
-
-		for (int i = 0; i < vector_size; i++)
-		{
-			Employee* e = &employee_list[i];
-
-			if (e->cl == find_cl)
-			{
-				find_list_.push_back(e);
-			}
-		}
-
-		break;
-
-	case Type::PhoneNum:
-
-		for (int i = 0; i < vector_size; i++)
-		{
-			Employee* e = &employee_list[i];
-
-			if (e->phoneNum == find_.content_)
-			{
-				find_list_.push_back(e);
-			}
-		}
-
-		break;
-	case Type::BirthDay:
-
-		for (int i = 0; i < vector_size; i++)
-		{
-			Employee* e = &employee_list[i];
-
-			if (e->birthday == find_.content_)
-			{
-				find_list_.push_back(e);
-			}
-		}
-
-		break;
-	case Type::Certi:
-
-		for (int i = 0; i < vector_size; i++)
-		{
-			Employee* e = &employee_list[i];
-
-			if (e->certi == find_certi)
-			{
-				find_list_.push_back(e);
-			}
-		}
-
-		break;
 	}
 
 	return find_list_;
@@ -167,57 +115,8 @@ vector<Employee*> Mod::getModifyList(vector<Employee>& employee_list)
 
 void Mod::modifyEmployeeInfo(vector<Employee*> modify_list)
 {
-	// [TO DO] Refactoring 
-	CareerLevel modify_cl;
-	vector<string> cl_type = { "CL1", "CL2", "CL3", "CL4" };
-	CertiLevel modify_certi;
-	vector<string> certi_type = { "ADV", "PRO", "EX" };
-
-	int cl_num = 0;
-	int certi_num = 0;
-
-	for (string cl_str : cl_type)
-	{
-		if (cl_str == modify_.content_)
-		{
-			modify_cl = static_cast<CareerLevel>(cl_num);
-		}
-		cl_num++;
-	}
-
-	for (string certi_str : certi_type)
-	{
-		if (certi_str == modify_.content_)
-		{
-			modify_certi = static_cast<CertiLevel>(certi_num);
-		}
-		certi_num++;
-	}
-
-
 	for (Employee* e : modify_list)
 	{
-		switch (modify_.type_)
-		{
-		case Type::EmployeeNum:
-			e->employeeNum = modify_.content_;
-			break;
-		case Type::Name:
-			e->name = modify_.content_;
-			break;
-		case Type::CL:
-			e->cl = modify_cl;
-			break;
-		case Type::PhoneNum:
-			e->phoneNum = modify_.content_;
-			break;
-		case Type::BirthDay:
-			e->birthday = modify_.content_;
-			break;
-		case Type::Certi:
-			e->certi = modify_certi;
-			break;
-		}
+		e->setColumnData(modify_.type_, modify_.content_);
 	}
-
 }
