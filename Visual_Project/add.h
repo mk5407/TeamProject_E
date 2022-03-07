22e5a97f1bafcde2220d9e739bef3bdf9b39e06c
PolarisCr <kimek12@gmail.com>
2022-03-07 10:34:10 +0900

    [Refactoring] Add Class Header Source 분리
    Add Class 의 Header 파일과 Source 파일을 분리하였습니다.
    
    Signed-off-by: PolarisCr <kimek12@gmail.com>

---
+++ Visual_Project/add.h
@@ -0,0 +1,20 @@
+#pragma once
+#include "Employee.h"
+#include "database.h"
+
+class IAdd
+{
+public:
+	virtual ~IAdd() {};
+	virtual void add(vector<string>& strVector) = 0;
+};
+
+class Add : public IAdd
+{
+public:
+	Add(IDatabase<Employee>* db);
+	void add(vector<string>& strVector);
+
+private:
+	IDatabase<Employee>* db;
+};
