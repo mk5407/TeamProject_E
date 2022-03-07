ec53b22f719cbf325e7812e200140841e16aae39
whit44 <kj.whit44@gmail.com>
2022-03-07 12:00:48 +0900

    [Feature] ColumnCkecer class 추가
    Signed-off-by: whit44 <kj.whit44@gmail.com>

---
+++ Visual_Project/columnChecker.cpp
@@ -0,0 +1,26 @@
+#include "columnChecker.h"
+
+ColumnChecker::ColumnChecker() {
+    IColumnEmployeeNum_ = new ColumnEmployeeNum();
+    IColumnName_ = new ColumnName();
+    IColumnPhoneNum_ = new ColumnPhoneNum();
+    IColumnCL_ = new ColumnCL();
+    IColumnBirthday_ = new ColumnBirthday();
+    IColumnCerti_ = new ColumnCerti();
+}
+
+ColumnChecker::~ColumnChecker() {
+    delete IColumnEmployeeNum_, IColumnName_, IColumnPhoneNum_,
+        IColumnCL_, IColumnBirthday_, IColumnCerti_;
+}
+
+bool ColumnChecker::columnCheck(vector<string> line) {
+    Employee emp(line[4], line[5], line[6], line[7], line[8], line[9]);
+
+    return (IColumnEmployeeNum_->IsValidColumn(emp)
+        && IColumnName_->IsValidColumn(emp)
+        && IColumnPhoneNum_->IsValidColumn(emp)
+        && IColumnCL_->IsValidColumn(emp)
+        && IColumnBirthday_->IsValidColumn(emp)
+        && IColumnCerti_->IsValidColumn(emp));
+}
---
+++ Visual_Project/columnChecker.h
@@ -0,0 +1,24 @@
+#pragma once
+#include <vector>
+#include <string>
+#include "column.h"
+
+using namespace std;
+
+class ColumnChecker
+{
+public:
+    ColumnChecker();
+    ~ColumnChecker();
+
+    bool columnCheck(vector<string> line);
+
+private:
+    Column* IColumnEmployeeNum_;
+    Column* IColumnName_;
+    Column* IColumnPhoneNum_;
+    Column* IColumnCL_;
+    Column* IColumnBirthday_;
+    Column* IColumnCerti_;
+};
+
