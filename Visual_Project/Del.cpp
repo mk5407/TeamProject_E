1aa0d4ce6525f57bc715c5fe81cb85f12e8521f4
whit44 <kj.whit44@gmail.com>
2022-03-08 11:12:23 +0900

    [BugFix] DEL 잘못된 index 참조 수정

--- Visual_Project/Del.cpp
+++ Visual_Project/Del.cpp
@@ -6,7 +6,8 @@ Del::Del(IDatabase<Employee>* database) {
 
 void Del::del(vector<int> delete_list) {
     int size = delete_list.size();
+    if (size <= 0) return;
     for (int index = size - 1; index >= 0; index--) {
-        m_pEmployee_database->delData(index);
+        m_pEmployee_database->delData(delete_list[index]);
     }
 }
